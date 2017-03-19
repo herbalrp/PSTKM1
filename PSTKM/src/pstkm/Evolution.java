f3b92e07333e2bf905b8618c3e5617930bedf9c4
Mateusz Zieliński <herbalrp@gmail.com>
2017-03-19 17:35:28 +0100

    Revert "Zmiany z seed, z krzyżowaniem"
    This reverts commit bd6756184803391cd5a82fa6b58f5108d9b32220.

--- PSTKM/src/pstkm/Evolution.java
+++ PSTKM/src/pstkm/Evolution.java
@@ -1,118 +1,52 @@
 package pstkm;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Evolution {
     
     List<Chromosome> ListOfChromosome;
-    List<Chromosome> ListOfTheBestChromosome = new ArrayList<Chromosome>();
     Integer PopulationSize;
     Integer NumberOfPopulations;
     float ProbabilityOfMutation;
     float ProbabilityOfCrossing;
     Net net;
     long seed;
-    int NumberOfMutations=0;
-    // cost=0;
-    int N=0;
-    String file;
-    Long time;
-    Random rand;
     
-    public Evolution(List<Chromosome> ListOfChromosome, Integer PopulationSize,  float ProbabilityOfMutation, float ProbabilityOfCrossing, long seed, Net net, String file)
+    public Evolution(List<Chromosome> ListOfChromosome, Integer PopulationSize, Integer NumberOfPopulations, float ProbabilityOfMutation, float ProbabilityOfCrossing, long seed, Net net)
     {
         this.ListOfChromosome=ListOfChromosome;        
         this.PopulationSize=PopulationSize;
+        this.NumberOfPopulations=NumberOfPopulations;
         this.ProbabilityOfMutation=ProbabilityOfMutation;
         this.ProbabilityOfCrossing=ProbabilityOfCrossing;
         this.net=net;
         this.seed=seed;
-        this.file=file;
-       rand = new Random(seed);
     }
     
-    public String startMaxTime(float numberOfMinutes)
+    public void start()
     {
-        long finish = System.currentTimeMillis()+(long)numberOfMinutes*60000; 
-        time=System.currentTimeMillis();
-        
-        while(System.currentTimeMillis() < finish)
-        {
-            cross();
-            mutate();
-            select();
-            
-        }
-
-        write();      
-        String s = "Zakończyłem pracę. Kolejne najlepsze wyniki zapisane zostały w pliku. \r\n";
-        s+=ListOfTheBestChromosome.get(ListOfTheBestChromosome.size()-1).toString();
-        return s;
-    }
-    
-      public String startNumberofRounds(int NumberOfPopulations)
-    {
-        time=System.currentTimeMillis();
         for(int i=0; i<NumberOfPopulations; i++)
         {
             cross();
             mutate();
             select();
-        
         }
 
-        write();      
-        String s = "Zakończyłem pracę. Kolejne najlepsze wyniki zapisane zostały w pliku. \r\n";
-        s+=ListOfTheBestChromosome.get(ListOfTheBestChromosome.size()-1).toString();
-        return s;
-        
-    }
-        public String startNumberOfMutations(int NumberOfMutations)
-    {
-        time=System.currentTimeMillis();
-        boolean continuation = true;
-        while(continuation)
-        {
-            cross();
-            continuation=mutateMax(NumberOfMutations);
-            select();
-        
+        System.out.println("The best solution:");
+        _writeBestSolutionToConsole(ListOfChromosome.get(0));
+        System.out.println("Total cost: "+ListOfChromosome.get(0).cost);
+        for(int k = 0; k <ListOfChromosome.get(0).ListOfTablesS.size(); k++) {
+         // System.out.println("Demand " + (k+1));
+         //   System.out.println("H: " +net.getListOfDemands().get(k).getdemandVolume());
+         //   System.out.println("Solution: ");
+            for(int l = 0; l<ListOfChromosome.get(0).ListOfTablesS.get(k).size(); l++) {
+             //   System.out.println(ListOfChromosome.get(0).ListOfTablesS.get(k).get(l));
+            }
         }
-
-        write();      
-         String s = "Zakończyłem pracę. Kolejne najlepsze wyniki zapisane zostały w pliku. \r\n";
-        s+=ListOfTheBestChromosome.get(ListOfTheBestChromosome.size()-1).toString();
-        return s;
-        
     }
-        
-          public String startLackOfDifference(int N)
-    {
-         time=System.currentTimeMillis();
-        boolean continuation = true;
-        while(continuation)
-        {
-            cross();
-            mutate();
-            continuation=select(N);
-          
-        }
-
-        write();    
-         String s = "Zakończyłem pracę. Kolejne najlepsze wyniki zapisane zostały w pliku. \r\n";
-        s+=ListOfTheBestChromosome.get(ListOfTheBestChromosome.size()-1).toString();
-        return s;
-        
-    }
-    
 
     private void _writeBestSolutionToConsole(Chromosome chromosome) {
         int x=1;
@@ -125,67 +59,34 @@ public class Evolution {
             System.out.println("|");
         }
     }
-    
-     private void _writeBestSolutionTofile(String file) {
-       
-          long time2=System.currentTimeMillis()-time;
-         File plik = new File(file);
-        try {
-            PrintWriter zapis = new PrintWriter(plik);
-            zapis.println("Długość trwania algorytmu: "+(double)(time2/60000)+" minut, a dokładnie: "+time2 + " ms");  
-            zapis.println("Najlepsze chromosomy w rundach:");           
-            int i=1;
-            for(Chromosome chromosome: ListOfTheBestChromosome)
-            {
-                zapis.println("Runda "+i);
-                zapis.println(chromosome.cost);
-                i++;
-            }
-            zapis.println("Ostateczny najlepszy chromosom:");    
-            zapis.println(ListOfTheBestChromosome.get(ListOfTheBestChromosome.size()-1).toString());    
-            zapis.println("Liczba lambd na linkach:");
-            zapis.println(CountLambdas((ListOfTheBestChromosome.get(ListOfTheBestChromosome.size()-1).ListOfTablesS)));
-            zapis.close();
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(Evolution.class.getName()).log(Level.SEVERE, null, ex);
-        }
-      
-    }
 
     private void cross() {
        
         Integer size = ListOfChromosome.size();
         Integer NumberOfPair=size/2;
-      
-        if(NumberOfPair>0)
-        {
-          for(int i=0; i<NumberOfPair; i++)
+        Random rand = new Random(seed);
+        
+        for(int i=0; i<NumberOfPair; i++)
         {
             float x = rand.nextFloat();
             if(x>ProbabilityOfCrossing)
             {
                 continue;
             }
-            int Index1 = rand.nextInt(ListOfChromosome.size()-1);
-            int Index2 = Index1;
-            while (Index2==Index1)
-            {
-                 Index2 = rand.nextInt(ListOfChromosome.size()-1);
-            }           
+            Integer Index1 = rand.nextInt(NumberOfPair-1);
+            Integer Index2 = NumberOfPair+rand.nextInt(NumberOfPair-1);
             
             List<Chromosome> Children = new ArrayList();
             Children=crossThisPair(ListOfChromosome.get(Index1).ListOfTablesS,ListOfChromosome.get(Index2).ListOfTablesS);
             ListOfChromosome.addAll(Children);
         }
-        }
-       
         
         
     }
 
     private void mutate() {
         
-    
+        Random rand = new Random(seed);
         
         for (int i=0; i<ListOfChromosome.size();i++)
         {
@@ -197,7 +98,6 @@ public class Evolution {
                     continue;
                 }
                 
-                
                 List<Integer> Gene = newGene(ListOfChromosome.get(i).ListOfTablesS.get(j));
                 ListOfChromosome.get(i).ListOfTablesS.set(j, Gene);
                 
@@ -205,34 +105,6 @@ public class Evolution {
         }
         
     }
-    
-     private boolean mutateMax(int max) {
-        
-       
-        
-        for (int i=0; i<ListOfChromosome.size();i++)
-        {
-            for(int j=0; j<ListOfChromosome.get(i).ListOfTablesS.size(); j++)
-            {
-                float x = rand.nextFloat();
-                if(x>ProbabilityOfMutation)
-                {
-                    continue;
-                }
-                if(NumberOfMutations>max)
-                {
-                    return false;
-                }
-                List<Integer> Gene = newGene(ListOfChromosome.get(i).ListOfTablesS.get(j));
-                ListOfChromosome.get(i).ListOfTablesS.set(j, Gene);
-                this.NumberOfMutations++;
-                return true;
-            }
-        }
-        return true;
-        
-    }
-    
 
     private void select() {
         
@@ -256,14 +128,6 @@ public class Evolution {
             ListOfChromosome.remove(i);
             i--;
         }
-        
-        if(ListOfChromosome.size()>0)
-        {
-          System.out.println(ListOfChromosome.get(0).cost);
-          Chromosome ch = ListOfChromosome.get(0);
-          ListOfTheBestChromosome.add(new Chromosome(ch.ListOfTablesS, ch.cost));  
-        }
-        
     }
     
       private float checkAndCountSolution(List<List> solution) {
@@ -301,41 +165,6 @@ public class Evolution {
        
         return cost;
     }
-      
-       private String CountLambdas(List<List> solution) {
-       float cost=0;
-       String s="";
-       List<Integer> lambdasOnLink = new ArrayList();
-       for(int i = 0; i<net.getListOfLinks().size(); i++) 
-       {
-             lambdasOnLink.add(0);
-       }
-       //pętla po zapotrzebowaniach
-       for(int i = 0; i< solution.size(); i++) {
-            //pętla po lambdach zapotrzebowania
-            for(int j = 0; j < solution.get(i).size(); j++) {
-                int numberOfLambdas = (int) solution.get(i).get(j);
-                List indexesOfLinks = net.getListOfDemands().get(i).getListOfPaths().get(j).getListOfLinks();
-                for(int k = 0; k< indexesOfLinks.size(); k++) {
-                    int index = (int)indexesOfLinks.get(k)-1;
-                    //dodanie lambd do listy
-                    lambdasOnLink.set(index, lambdasOnLink.get(index) + numberOfLambdas);
-                }
-            }
-        }
-        //wyliczenie kosztu, wypisanie linku i przypisanej liczby lambd
-        for(int l = 0; l<lambdasOnLink.size(); l++) {
-            s+="Link: " +(l+1)+"\r\n";
-            s+="    liczba lambd: " +lambdasOnLink.get(l)+"\r\n";
-            Float costOfLink = count(lambdasOnLink.get(l),net.getListOfLinks().get(l).getNumberOfLambdas(),net.getListOfLinks().get(l).getCostOfFibre());
-            cost+=costOfLink;
-            s+="    Koszt: " +costOfLink+"\r\n";
-           // System.out.println("Max number of lambdas on link: " +(net.getListOfLinks().get(l).getNumberOfLambdas() * net.getListOfLinks().get(l).getNumberOfFibres()));
-           
-        }
-       
-        return s;
-    }
     
       private Float count(Integer get, Integer numberOfLambdas, Float costOfFibre) {
         
@@ -352,28 +181,26 @@ public class Evolution {
 
     private List<Chromosome> crossThisPair(List<List> Chromosome1, List<List> Chromosome2) {
         
-       
-        List<List> Chromosome1k = copy(Chromosome1);
-        List<List> Chromosome2k = copy(Chromosome2);
-        Integer NumberOfChanges=rand.nextInt(Chromosome1k.size()-1)+1;
-                 
+        Random rand = new Random(seed);
+        Integer NumberOfChanges=rand.nextInt(Chromosome1.size()-1);
+         
         for(int i=0; i<NumberOfChanges; i++)
         {
-            Integer IndexOfGene=rand.nextInt(Chromosome1k.size()-1);
-            List<Integer> Gene1 = Chromosome1k.get(IndexOfGene);
-            Chromosome1k.set(IndexOfGene, Chromosome2k.get(IndexOfGene));
-            Chromosome2k.set(IndexOfGene, Gene1);
+            Integer IndexOfGene=rand.nextInt(Chromosome1.size()-1);
+            List<Integer> Gene1 = Chromosome1.get(IndexOfGene);
+            Chromosome1.set(IndexOfGene, Chromosome2.get(IndexOfGene));
+            Chromosome2.set(IndexOfGene, Gene1);
         }
         
         List<Chromosome> Children = new ArrayList();
-        Children.add(new Chromosome (Chromosome1k));
-        Children.add(new Chromosome(Chromosome2k));
+        Children.add(new Chromosome (Chromosome1));
+        Children.add(new Chromosome(Chromosome2));
         return Children;
     }
 
     private List<Integer> newGene(List Gene) {
         
-       
+        Random rand = new Random(seed);
         Integer sum=0;
         for(int i=0;i<Gene.size();i++)
         {
@@ -398,80 +225,5 @@ public class Evolution {
         Gene.set(Gene.size()-1, sum);
         
         return Gene;
-    }
-
-    private void write() {
-         System.out.println("The best solution:");
-        _writeBestSolutionToConsole(ListOfChromosome.get(0));
-        _writeBestSolutionTofile(file);
-        System.out.println("Total cost: "+ListOfChromosome.get(0).cost);
-    }
-
-    private boolean select(int Nx) {
-           for(int i=0; i<ListOfChromosome.size();i++)
-        {
-            float cost = checkAndCountSolution(ListOfChromosome.get(i).ListOfTablesS);
-            if(cost==-1)
-            {
-                ListOfChromosome.remove(i);
-                i--;
-            } else
-            {
-                ListOfChromosome.get(i).cost=cost;
-            }
-        }
-        
-        Collections.sort(ListOfChromosome);
-        
-        for(int i=PopulationSize; i<ListOfChromosome.size(); i++)
-        {
-            ListOfChromosome.remove(i);
-            i--;
-        }
-        
-        if(ListOfTheBestChromosome.size()>0)
-        {
-           float costT = ListOfTheBestChromosome.get(ListOfTheBestChromosome.size()-1).cost;  
-            if(ListOfChromosome.get(0).cost< costT)
-        {
-            N=0;
-        } else
-        {
-            N++;
-        }
-        if(N>Nx)
-        {
-            return false;
-        }
-        }
-       
-        
-        if(ListOfChromosome.size()>0)
-        {
-            System.out.println(ListOfChromosome.get(0).cost);
-           Chromosome ch = ListOfChromosome.get(0);
-          ListOfTheBestChromosome.add(new Chromosome(ch.ListOfTablesS, ch.cost));  
-        }
-        
-       
-        return true;
-    }
-
-    private List<List> copy(List<List> Chromosome1) {
-        List<List> list = new ArrayList();
-        
-        for(int i=0; i<Chromosome1.size(); i++)
-        {
-            List list2 = new ArrayList();
-            
-            for(int j=0; j<Chromosome1.get(i).size(); j++)
-            {
-                list2.add(Chromosome1.get(i).get(j));
-            }
-            
-            list.add(list2);
-            
-        }
-        return list;
     }
 }
